a9411c4a3165bed79f2ac27c9fec945bfdd8f9dc
timothy-tianmu <tianmu_19970924@163.com>
2019-04-21 16:59:48 +0800

    implement canvas pen/eraser

--- 02_transforms/transforms.js
+++ 02_transforms/transforms.js
@@ -1,27 +1,68 @@
 class TransformCanvas extends Canvas {
   constructor(options) {
     super(options)
-    this.x = 50
-    this.y = 20
-    this.interval = 20
+    this.pixelWidth = 50
+    this.pixelHeight = 20
+    this.arrowWidth = this.pixelWidth * 3
+    this.arrowHeight = this.pixelHeight * 5
+    this.x = this.arrowWidth / 2
+    this.y = this.arrowHeight / 2
+    this.offsetX = this.x
+    this.offsetY = this.y
+    this.setupActions()
   }
 
-  update() {
-    if (this.y < this.H) {
-      this.y += this.interval
-    }
+  setupActions() {
+    this.canvas.addEventListener("mousemove", (ev) => {
+      this.offsetX = ev.offsetX
+      this.offsetY = ev.offsetY
+    })
   }
-  drawLine(x1, y1, x2, y2, color = "#000", lineWidth = 1) {
+  update() {
+    var easing = 0.05
+    var dx = this.offsetX - this.x 
+    var dy = this.offsetY - this.y
+    if (Math.abs(dx) < 5 && Math.abs(dy) < 5) {
+      return
+    }
+    this.x += dx * easing
+    this.y += dy * easing
+  }
+  genColor() {
+    var color = []
+    for (var i = 0; i < 3; i++) {
+      var n = Math.random() * 255
+      color.push(n)
+    }
+    var r = color[0]
+    var g = color[1]
+    var b = color[2]
+    return `RGB(${r}, ${g}, ${b})`
+  }
+
+  drawArrow() {
+    var x = this.x - this.arrowWidth / 2 
+    var y = this.y - this.arrowHeight / 2
+    for (let i = 0; i < 5; i++) {
+      this.ctx.save()
+      this.ctx.fillStyle = this.genColor()
+      this.ctx.fillRect(x, y + i * this.pixelHeight, this.pixelWidth, this.pixelHeight)
+      this.ctx.restore()
+    }
+    for (let i = 0; i < 3; i++) {
+      this.ctx.save()
+      this.ctx.fillStyle = this.genColor()
+      this.ctx.fillRect(x + this.pixelWidth, y +(i+1) * this.pixelHeight, this.pixelWidth, this.pixelHeight)
+      this.ctx.restore()
+    }
     this.ctx.save()
-    this.ctx.strokeStyle = color
-    this.ctx.lineWidth = lineWidth
-    this.ctx.moveTo(x1, y1)
-    this.ctx.lineTo(x2, y2)
-    this.ctx.stroke()
+    this.ctx.fillStyle = this.genColor()
+    this.ctx.fillRect(x + this.pixelWidth * 2, y+this.pixelHeight * 2, this.pixelWidth, this.pixelHeight)
     this.ctx.restore()
   }
 
   draw() {
-    this.drawLine(this.x, this.y, this.x + 100, this.y)
+    this.ctx.clearRect(0, 0, this.W, this.H)
+    this.drawArrow()
   }
 }
--- 03_drawBoard/drawBoard.js
+++ 03_drawBoard/drawBoard.js
@@ -1,29 +1,85 @@
+const PEN = "PEN"
+const ERASER = "ERASER"
+const RECT = "RECT"
 class DrawBoard extends Canvas {
   constructor(options) {
     super(options)
     this.setup()
+    this.setupTools()
+  }
+
+  setupTools() {
+    this.tool = PEN
+    var penBtn = document.getElementById("pen")
+    var eraserBtn = document.getElementById("eraser")
+    var rectBtn = document.getElementById("rect")
+    penBtn.addEventListener("click", (ev) => {
+      this.tool = PEN
+    })
+    eraserBtn.addEventListener("click", (ev) => {
+      this.tool = ERASER
+    })
+    rectBtn.addEventListener("click", (ev) => {
+      this.tool = RECT
+    })
+  }
+
+  updateMouseOffset(mouseEvent) {
+    var {offsetX, offsetY} = mouseEvent
+    this.offsetX = offsetX
+    this.offsetY = offsetY
   }
 
   setup() {
-    this.registerMouseAction("mousedown", (ev) => {
-      this.lastPos = ev.pos
+    var b = this
+    this.canvas.addEventListener("mousedown", function(ev) {
+      b.updateMouseOffset(ev)
+      b.isMouseDown = true
     })
-    this.registerMouseAction("mousemove", (ev) => {
-      var pos = ev.pos
-      if (this.isMouseDown) {
-        this.drawLine(this.lastPos, pos)
-        this.lastPos = pos
+    this.canvas.addEventListener("mousemove", function(ev) {
+      var lastOffsetX = b.offsetX
+      var lastOffsetY = b.offsetY
+      b.updateMouseOffset(ev)
+      if (!b.isMouseDown) {
+        return
       }
+      if (b.tool === PEN) {
+        b.drawLine(lastOffsetX, lastOffsetY, b.offsetX, b.offsetY)
+      } else if (b.tool === ERASER) {
+        b.ctx.clearRect(b.offsetX, b.offsetY, 20, 20)
+      }
+    })
+    this.canvas.addEventListener("mouseup", function(ev) {
+      b.isMouseDown = false
     })
   }
 
-  drawLine(lastPos, newPos) {
+  drawLine(x1, y1, x2, y2) {
     this.ctx.save()
     this.ctx.lineWidth = 1.5
     this.ctx.strokeStyle = "#000"
-    this.ctx.moveTo(lastPos.x, lastPos.y)
-    this.ctx.lineTo(newPos.x, newPos.y)
+    this.ctx.beginPath()
+    this.ctx.moveTo(x1, y1)
+    this.ctx.lineTo(x2, y2)
     this.ctx.stroke()
     this.ctx.restore()
+  }
+
+  update() {
+    super.update()
+    log(this.tool)
+  }
+
+  mount(options) {
+    super.mount(options)
+    var root = document.getElementById(this.rootId)
+    var toolPane = `
+      <div id="toolPane">
+        <button id="eraser">ERASER</button>
+        <button id="pen">PEN</button>
+        <button id="rect">RECT</button>
+      </div>
+    `
+    root.insertAdjacentHTML("afterbegin", toolPane)
   }
 }
